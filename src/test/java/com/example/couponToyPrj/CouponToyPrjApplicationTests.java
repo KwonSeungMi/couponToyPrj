7d493965e99b6e8c254ececa6a626cbc470650b4
KwonSeungMi <ksm97812@gmail.com>
2020-10-28 12:48:37 +0900

    Init Source

---
+++ src/test/java/com/example/couponToyPrj/CouponToyPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couponToyPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponToyPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
